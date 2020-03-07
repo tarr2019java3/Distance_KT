9a44df79615aac4730a83372d79a28a8a1fd32ca
Trzaska <euroekspertkrystian@gmail.com>
2020-03-07 10:46:27 +0100

    done

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Point p1 = new Point(9,3);
+        Point p2 = new Point(19,29);
+
+        System.out.println("Odległość pomiędzy p1 a p2 jest = " + p1.distance(p2));
+
+
+    }
+}
